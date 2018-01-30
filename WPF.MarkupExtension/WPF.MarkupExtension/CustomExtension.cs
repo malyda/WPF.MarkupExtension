a14ecb3bdc7c9461747f3833845511ba36f58ed6
David malý <maly.david.it@gmail.com>
2018-01-30 10:53:10 +0100

    Init

---
+++ WPF.MarkupExtension/WPF.MarkupExtension/CustomExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF.MarkupExtension
+{
+    public class CustomExtension : DependencyObject
+    {
+        public static readonly DependencyProperty ItemProperty =
+            DependencyProperty.RegisterAttached("ItemProperty", typeof(Item), typeof(CustomExtension), new FrameworkPropertyMetadata(null) );
+
+        public static void SetItemProperty(UIElement element, Item value)
+        {
+            element.SetValue(ItemProperty, value);
+        }
+
+        public static Item GetItemProperty(UIElement element)
+        {
+            return (Item)element.GetValue(ItemProperty);
+        }
+    }
+}
---
+++ WPF.MarkupExtension/WPF.MarkupExtension/Item.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.MarkupExtension
+{
+   public class Item
+    {
+        public string Name { get; set; }
+
+        public Item() { }
+
+        public Item(string name)
+        {
+            Name = name;
+        }
+    }
+}
